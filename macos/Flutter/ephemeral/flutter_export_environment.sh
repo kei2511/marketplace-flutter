09240eba8966e07c2cd6dc21597d14369ed91898
muhammadfaizramadha <mfaizschl@gmail.com>
2025-05-15 23:26:57 +0700

    Template Flutter

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\FOKUS\BELAJAR\FLUTTER\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\FOKUS\BELAJAR\DART\marketplace-flutter\flutter_application_1"
+export "FLUTTER_APPLICATION_PATH=D:\FOKUS\BELAJAR\DART\marketplace-flutter\marketplace_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
